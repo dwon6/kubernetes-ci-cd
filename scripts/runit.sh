4f70125b829e7f5dbcac24370669670d9f3b7c46
mchalep <patrick.mchale.mail@gmail.com>
2019-10-14 07:29:57 +0000

    Added File

---
+++ scripts/runit.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+trap "set +x; sleep 5; set -x" DEBUG
+helm install stable/etcd-operator --version 0.8.0 --name etcd-operator --set customResources.createEtcdClusterCRD=false --debug --wait
+helm upgrade --set customResources.createEtcdClusterCRD=true etcd-operator stable/etcd-operator
+
+kubectl create -f manifests/etcd-cluster.yaml
+
+kubectl create -f manifests/etcd-service.yaml
+
+kubectl apply -f manifests/all-services.yaml
+
+docker build -t 127.0.0.1:30400/monitor-scale:`git rev-parse --short HEAD` -f applications/monitor-scale/Dockerfile applications/monitor-scale
+
+#docker build -t socat-registry -f applications/socat/Dockerfile applications/socat
+
+docker stop socat-registry; docker rm socat-registry; docker run -d -e "REG_IP=`minikube ip`" -e "REG_PORT=30400" --name socat-registry -p 30400:5000 socat-registry
+
+#lsof -i :30400
+
+docker push 127.0.0.1:30400/monitor-scale:`git rev-parse --short HEAD`
+
+docker stop socat-registry
+
+#minikube service registry-ui
+
+kubectl apply -f manifests/monitor-scale-serviceaccount.yaml
+
+sed 's#127.0.0.1:30400/monitor-scale:$BUILD_TAG#127.0.0.1:30400/monitor-scale:'`git rev-parse --short HEAD`'#' applications/monitor-scale/k8s/deployment.yaml | kubectl apply -f -
+
+kubectl rollout status deployment/monitor-scale
+
+#kubectl get pod monitor-scale-6d8744f69f-fmtx5 --output=yaml
+
+#kubectl get pods
+#kubectl get services
+#kubectl get ingress
+#kubectl get deployments
+
+scripts/puzzle.sh
+
+kubectl rollout status deployment/puzzle
+kubectl rollout status deployment/mongo
+
+scripts/kr8sswordz-pages.sh
+
+kubectl rollout status deployment/kr8sswordz
+
+#kubectl get pods
+
+#minikube service kr8sswordz
